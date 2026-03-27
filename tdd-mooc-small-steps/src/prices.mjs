c243772a8a4999394dc3d76ed33be5834b810681
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2026-03-27 13:09:55 +0200

    Add date2 variable

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -19,6 +19,7 @@ function createApp(database) {
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
     const date = parseDate(req.query.date);
+    const date2 = '';
     const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
