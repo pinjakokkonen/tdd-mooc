4bb54c4264b0dac44d03ab975b0916f76639e8d9
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2026-03-27 15:21:56 +0200

    Make holiday use a Temporal.Plaindate

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -90,7 +90,7 @@ function createApp(database) {
   function isHoliday(date) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
-      let holiday = convert(new Date(row.holiday));
+      let holiday = Temporal.PlainDate.from(row.holiday);
       if (
         date &&
         date.year === holiday.year &&
