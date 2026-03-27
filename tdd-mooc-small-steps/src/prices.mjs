e5406c1729034a262f59e2ea8f60da20b01754fc
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2026-03-27 14:50:05 +0200

    Initial state

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -18,14 +18,14 @@ function createApp(database) {
     const age = req.query.age ? parseInt(req.query.age) : undefined;
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
-    const date = parsePlainDate(req.query.date);
+    const date = parseDate(req.query.date);
     const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
 
-  function parsePlainDate(dateString) {
+  function parseDate(dateString) {
     if (dateString) {
-      return Temporal.PlainDate.from(dateString);
+      return new Date(dateString);
     }
   }
 
@@ -52,7 +52,6 @@ function createApp(database) {
 
   function calculateCostForDayTicket(age, date, baseCost) {
     let reduction = calculateReduction(date);
-  
     if (age === undefined) {
       return Math.ceil(baseCost * (1 - reduction / 100));
     }
@@ -77,18 +76,18 @@ function createApp(database) {
   }
 
   function isMonday(date) {
-    return date.dayOfWeek === 1;
+    return date.getUTCDay() === 1;
   }
 
   function isHoliday(date) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
-      let holiday = Temporal.PlainDate.from(row.holiday);
+      let holiday = new Date(row.holiday);
       if (
         date &&
-        date.year === holiday.year &&
-        date.month === holiday.month &&
-        date.day === holiday.day
+        date.getFullYear() === holiday.getFullYear() &&
+        date.getMonth() === holiday.getMonth() &&
+        date.getDate() === holiday.getDate()
       ) {
         return true;
       }
