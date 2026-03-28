9939ac6dc09e84da6c5932b5d3319da937ea5df9
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2026-03-28 16:51:59 +0200

    Rotating 3x3 shape can be rotated right/clockwise

--- tdd-mooc-tetris/src/RotatingShape.mjs
+++ tdd-mooc-tetris/src/RotatingShape.mjs
@@ -8,6 +8,16 @@ export class RotatingShape {
     return new RotatingShape(grid);
   }
 
+  rotateRight() {
+    let newGrid = new Array(this.grid.length);
+    for (let row=0; row<this.grid.length; row++) {
+        newGrid[row] = new Array(this.grid[0].length);
+      for (let column=0; column<this.grid[0].length; column++) {
+        newGrid[row][column] = this.grid[this.grid[0].length-column-1][row];}
+    }
+    return new RotatingShape(newGrid);
+  }
+
   toString() {
     return this.grid.map(row => row.join('')).join('\n') + '\n';
   }
