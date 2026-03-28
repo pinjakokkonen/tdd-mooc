409fb86aab78e0d91d359ea748b7617cac11628f
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2026-03-28 13:30:59 +0200

    When a block is dropped it starts from the top middle

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -1,17 +1,26 @@
 export class Board {
   width;
   height;
+  block;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
   }
 
+  drop(block) {
+    this.block = block;
+  }
+
   toString() {
     let string = '';
     for (let row=1; this.height>=row; row++) {
       for (let column=1; this.width>=column; column++) {
-        string += '.';
+        if (row===1 && column===2 && this.block) {
+          string += this.block;
+        } else {
+          string += '.';
+        }
       }
       string += '\n';
     }
