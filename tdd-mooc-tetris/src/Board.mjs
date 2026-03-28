3bb501e54e98b818bcfe4bd2e95c311acba72d01
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2026-03-28 13:44:02 +0200

    When a block is dropped it moves down one row per tick

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -2,6 +2,9 @@ export class Board {
   width;
   height;
   block;
+  blockRow;
+  blockColumn;
+
 
   constructor(width, height) {
     this.width = width;
@@ -10,13 +13,19 @@ export class Board {
 
   drop(block) {
     this.block = block;
+    this.blockRow = 1;
+    this.blockColumn = 2;
+  }
+
+  tick() {
+    this.blockRow += 1;
   }
 
   toString() {
     let string = '';
     for (let row=1; this.height>=row; row++) {
       for (let column=1; this.width>=column; column++) {
-        if (row===1 && column===2 && this.block) {
+        if (row===this.blockRow && column===2 && this.block) {
           string += this.block;
         } else {
           string += '.';
