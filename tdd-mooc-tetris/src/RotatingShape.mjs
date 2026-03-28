36f8fed4721494e4ebaf19f616e5707a46c7d9dd
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2026-03-28 16:56:36 +0200

    Rotating 3x3 shape can be rotated left/counter-clockwise

--- tdd-mooc-tetris/src/RotatingShape.mjs
+++ tdd-mooc-tetris/src/RotatingShape.mjs
@@ -13,7 +13,16 @@ export class RotatingShape {
     for (let row=0; row<this.grid.length; row++) {
         newGrid[row] = new Array(this.grid[0].length);
       for (let column=0; column<this.grid[0].length; column++) {
-        newGrid[row][column] = this.grid[this.grid[0].length-column-1][row];}
+        newGrid[row][column] = this.grid[this.grid[0].length-column-1][row];
+      }
+    }
+    return new RotatingShape(newGrid);
+  }
+  rotateLeft() {
+    let newGrid = new Array(this.grid.length);
+    for (let row=0; row<this.grid.length; row++) {
+        newGrid[row] = new Array(this.grid[0].length);
+      for (let column=0; column<this.grid[0].length; column++) { newGrid[row][column] = this.grid[column][this.grid.length-row-1]; }
     }
     return new RotatingShape(newGrid);
   }
